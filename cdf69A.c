feeb0080d75659f9f566b96ecc880662872d4c4e
Sourav1704093 <42141718+Sourav1704093@users.noreply.github.com>
2018-08-06 18:55:37 +0600

    Add files via upload
    Codeforces 69A solution

---
+++ cdf69A.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, x, y, z, xsum= 0, ysum= 0, zsum = 0;
+    scanf("%d",&n);
+
+    while (n--)
+    {
+        scanf("%d %d %d",&x,&y,&z);
+        xsum += x;
+        ysum += y;
+        zsum += z;
+    }
+
+    if (xsum == 0 && ysum == 0 && zsum == 0)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+       printf("NO\n");
+    }
+
+    return 0;
+}
